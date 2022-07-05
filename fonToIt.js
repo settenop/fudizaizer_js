8656629d27557408f741a71c2ee143958feaa364
Settimo Augusto Vittorio <settenop@gmail.com>
2022-07-05 14:51:51 +0200

    impruved neims fudizescion

---
+++ fonToIt.js
@@ -0,0 +1,54 @@
+window.fudizaizerFonToIt = {
+    "ð": "d",    //   lea[th]er
+    "e": "e",    //   [e]nd, br[ea]d
+    "ɑ": "a",    //   [a]rm
+    "d": "d",    //   [d]ad
+    "v": "v",    //   fi[v]e
+    "k": "k",   //   [k]it, [c]at
+    "s": "s",    //   [s]it
+    "w": "w",    //   [w]ith
+    "ʊ": "u",    //   w[ou]ld
+    "l": "l",    //   [l]ive
+    "f": "f",    //   [f]ine
+    "ɐ": "a",    //   
+    "b": "b",    //   [b]ug
+    "æ": "e",    //   c[a]t
+    "ə": "e",    //   [a]bout
+    "ɪ": "i",    //   [e]ngland
+    "z": "z",    //   bu[zz]
+    "n": "n",    //   [n]et
+    "ŋ": "ng",   //   to[ngue]
+    "m": "m",    //   [m]an
+    "t": "t",    //   [t]ip
+    "ʃ": "sh",   //   o[ce]an
+    "ɛ": "e",    //   
+    "i": "i",    //   p[eo]ple
+    "ɹ": "r",    //   [r]un
+    "ɒ": "o",    //   [ho]nest
+    "ʌ": "a",    //   d[ou]ble
+    "h": "h",    //   [h]ope
+    "ɔ": "o",    //   
+    "a": "a",    //  
+    "ɡ": "gh",   //   
+    "ʒ": "z",    //   a[z]ure
+    "j": "i",    //   on[i]on
+    "u": "u",    //   
+    "p": "p",    //   [p]in
+    "θ": "f",    //   ba[th]
+    "ɜ": "e",    //   b[i]rd
+    "r": "r",    //   [r]un
+    "ʲ": "",     //   platalizeshon modifaier
+    "x": "c",    //   boo[k]
+    "̃": "",
+    "ɬ": "", 
+    " ": "", 
+    "ː": "",
+    ":": "",
+    "/": "",
+    "ˈ": "",
+    "ˌ": "",
+    "ə‍ʊ": "ou",
+    "ət": "ert",
+    "ə‍l": "ol",
+    "ən": "on"
+};
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function FudizaizNeim(neim) {
-    // jast an ixempol, somfing smarter is on de wey
-    if (neim === "valueOf") {
-        return neim = "veliuOf";
+const italianaizSortedChiis = Object.keys(window.fudizaizerFonToIt).sort((a, b) => b.length - a.length);
+
+function capitalaiz(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function ItalianaizFon(uord) {
+    for (let key of italianaizSortedChiis) {
+        uord = uord.replaceAll(key, window.fudizaizerFonToIt[key]);
     }
-    return neim;
+    return uord;
+}
+
+function FudizaizNeim(neim) {
+    let words = neim.split(/(?=[A-Z])/).map(w => w.replace(/_/g, ""));
+    for (let i = 0; i < words.length; ++i) {
+        const luking4 = words[i].toLowerCase();
+        if (fudizaizerEnTuFon.hasOwnProperty(luking4)) {
+            words[i] = fudizaizerEnTuFon[luking4]
+        }
+    }
+    console.log(neim);
+    console.log(words.join(""));
+    words = words.map(ItalianaizFon).map(capitalaiz);
+    if (neim[0] < 'A' || neim[0] > 'Z') {
+        words[0] = words[0].toLowerCase();
+    }
+    return words.join("");
 }
 
 function FudizaizObject(obj) {
@@ -11,6 +33,7 @@ function FudizaizObject(obj) {
     for (var chi of chiis) {
         var betterNeim = FudizaizNeim(chi);
         if (betterNeim !== chi) {
+            console.log(betterNeim);
             obj[betterNeim] = obj[chi];
         }
     }
@@ -22,4 +45,14 @@ function FudizaizEnvironment() {
     window.turiVannia = console.error;
     
     FudizaizObject(window.Object.prototype);
+}
+
+function GetRiquairdFonems() {
+    var fonems = new Set();
+    for (var val of Object.values(window.fudizaizerEnTuFon)) {
+        for (var fonem of val.replace(/['\/ˈ,ˌ:]/g, "")) {
+            fonems.add(fonem);
+        }
+    }
+    return fonems;
 }
